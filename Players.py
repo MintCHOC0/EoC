c55867b44fa718f4c44ed95b823b9b9e6bc207b9
MintCHOC0 <korean13579@gmail.com>
2023-03-13 20:48:12 +0900

    Player class 4종 추가
    Friedman: 상대가 배신할 시 무조건 배신
    Joss: TitForTat과 동일하지만, 10% 확률로 선제 배신
    XOR: 상대의 마지막 선택의 역을 선택 (ex. 상대가 협력 시, 나는 배신)
    BetrayExp: 상대가 협력을 많이 할수록 배신 확률 증가
    
    Friedman, Joss의 경우에는 모티브가 되었던 대회의 실제 전략을 모방

--- Players.py
+++ Players.py
@@ -1,17 +1,26 @@
-from random import randrange
-
+from random import randrange, random
+from math import exp, exp2
 """
 chooseChoice() function of all classes is affected only by
 current own score and list_choice,
 current opponent's score and list_choice variables.
 (Assume that Class dosen't know the result of previous opponent or future result.)
 """
+class TestClass:
+    def __init__(self):
+        self.point = 0
+        self.list_choice = []
+
+
 class DefaultClass:
     def __init__(self):
         self.point = 0
         self.list_choice = []
     
-    def chooseChoice(self):
+    def __len__(self):
+        return len(self.list_choice)
+
+    def chooseChoice(self, opponent):
         pass
 
 
@@ -35,3 +44,41 @@ class TitForTat(DefaultClass):
     def chooseChoice(self, opponent):
         if len(opponent.list_choice) < 1: return 1
         else: return opponent.list_choice[-1]
+
+
+class FriedMan(DefaultClass):
+    def chooseChoice(self, opponent):
+        if 0 in opponent.list_choice: return 0
+        else: return 1
+
+
+class Joss(DefaultClass):
+    def chooseChoice(self, opponent):
+        if len(opponent.list_choice):
+            if (random() < 0.1): return 0
+            else: return opponent.list_choice[-1]
+        else: return 1
+
+
+class XOR(DefaultClass):
+    def chooseChoice(self, opponent):
+        if (len(opponent.list_choice)):
+            return not(opponent.list_choice[-1])
+
+
+class BetrayExp(DefaultClass):
+    def getStack(self, opponent):
+        stack = 0
+        length = len(opponent)
+        for i in range(length):
+            if opponent.list_choice[length - i - 1] == 0: break
+            stack+=1
+        return stack
+    
+    def formula(self, stack):
+        return (exp2(stack/8) - 1) / 4
+    
+    def chooseChoice(self, opponent):
+        stack = self.getStack(opponent)
+        if self.formula(stack) > random(): return 0
+        else: return 1
